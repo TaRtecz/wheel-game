394f3712d5b0cbd6111c0cab9a74c0b4c4bc08ce
Andrey Palatov <big.dok.000@gmail.com>
2025-08-27 01:44:19 +0500

    added workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/wheel-game/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
